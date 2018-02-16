4e797fa6f9efa875e79d1e36b66b7a4bac2d56b7
Gabriel de O. Perdomo <goperdomo@gmail.com>
2018-02-16 15:50:35 +0100

    Implement express

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const config = require('./config/config');
+const routes = require('./routes/index');
+
+// Set pug as template engine
+app.set('view engine', 'pug');
+
+// Use routes
+app.use("/", routes);
+
+// Catch 404 Error
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  if (!err.status) err.status = 500
+  res.locals.error = err;
+  res.status(err.status).json(err.message);
+});
+
+// Listen to port
+app.listen(config.port, () => {
+  console.log(`Listening on port ${config.port}`);
+});
