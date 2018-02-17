30026662e2aff2991f8b979412e244ef7f93eff0
Gabriel de O. Perdomo <goperdomo@gmail.com>
2018-02-17 14:23:40 +0100

    Implement views and split the data

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+// Require data
+const overview = require('../data/overview'),
+      players = require('../data/players'),
+      needs = require('../data/needs'),
+      schedule = require('../data/schedule'),
+      footer = require('../data/footer');
+
+// Middleware for all routes
+router.use((req, res, next) => {
+  res.locals.footer = footer;
+  next();
+});
+
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', overview);
 });
+
 router.get('/players', (req, res) => {
-  res.render('players');
+  res.render('players', players);
 });
+
 router.get('/needs', (req, res) => {
-  res.render('needs');
+  res.render('needs', needs);
 });
+
 router.get('/schedule', (req, res) => {
-  res.render('schedule');
+  res.render('schedule', schedule);
 });
 
 module.exports = router;
